dfef4f9c091faea16c6468f13edd18b33544b82e
TiagoToi <tiago.v.toi@gmail.com>
2025-10-09 16:31:31 -0300

    rota servidor certa

--- Karaoke_Frontend/src/services/api.js
+++ Karaoke_Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  	baseURL: 'http://localhost:3000', 
+  	baseURL: 'https://karaokemax.onrender.com', 
 });
 
 // Interceptor para adicionar token automaticamente
