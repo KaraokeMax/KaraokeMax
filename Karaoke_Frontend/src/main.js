387c379d2db1e633796bd6a36eb3eaf8aba8135f
João Victor De Mauro Vasconcellos <Jvictormv@gmail.com>
2025-11-04 15:42:02 -0300

    tela AdicionarMusica (função de busca + design)

--- Karaoke_Frontend/src/main.js
+++ Karaoke_Frontend/src/main.js
@@ -2,10 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 // importa a tela de login
-import TelaLogin from './views/TelaLogin.vue'
-import TelaPrincipal from './views/TelaPrincipal.vue'
-import TelaDefinirSenha from './views/TelaDefinirSenha.vue'
-import TelaKaraoke from './views/TelaKaraoke.vue'
 import router  from './router'
 
 const app = createApp(App)
